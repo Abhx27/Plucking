7daae4365d4534724c2775c0fb8352ad326de52a
Abhx27 <68250765+Abhx27@users.noreply.github.com>
2020-10-02 17:16:51 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,39 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	rock = new rock(100,100)
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  strockWeight(4);
+  rock.display();
+  
+  drawSprites();
+ 
+}
+
+
+
